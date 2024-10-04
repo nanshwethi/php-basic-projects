43f97c4541c3ae67a6685a7c487f12844a308397
nanshwethi <104611636+nanshwethi@users.noreply.github.com>
2024-10-04 22:24:45 +0630

    Update create_product.php

--- create_product.php
+++ create_product.php
@@ -44,7 +44,7 @@
                         <input type="number" id="price" required name="price" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="eg.400">
                     </div>
                     <div class=" max-w-sm">
-                        <label for="price" class="block text-sm font-medium mb-2 dark:text-white">Rating</label>
+                        <label for="rating" class="block text-sm font-medium mb-2 dark:text-white">Rating</label>
 
                         <select name="product_rating" class="py-3 px-4 pe-9 block w-full bg-white border-transparent  rounded-lg text-sm focus:border-blue-500 focus:ring-gray-500 focus-within:outline-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:border-transparent dark:text-neutral-400 dark:focus:ring-neutral-600">
 
